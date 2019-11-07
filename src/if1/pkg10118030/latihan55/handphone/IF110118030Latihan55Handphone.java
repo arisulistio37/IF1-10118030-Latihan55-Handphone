22650afb7148141a147b1a31824580bb280b3834
arisulistio37 <arisulistio37@gmail.com>
2019-11-07 19:51:11 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan55/handphone/IF110118030Latihan55Handphone.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan55.handphone;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi program : program ini berisi tentang daftar handphone
+ */
+public class IF110118030Latihan55Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Android android = new Android("Samsung", "Android", "Grand", 3000000);
+        android.setKeyStore("234ibfd3840fo");
+        android.displayProduct();
+        System.out.println("Android Key Store : "+android.getKeyStore());
+        
+        System.out.println("");
+        BlackBerry bb = new BlackBerry("BlackB", "RIM", "Curve", 2000000);
+        bb.setPinBB("BHS249");
+        bb.displayProduct();
+        System.out.println("PIN : "+bb.getPinBB());
+        
+        System.out.println("");
+        WindowsPhone nokia = new WindowsPhone("Nokia", "Win8", "Lumia", 1000000);
+        nokia.setWpKeyStore("UUUQIJWORJ");
+        nokia.displayProduct();
+        System.out.println("Wp Key Store : "+nokia.getWpKeyStore());
+    }
+    
+}
